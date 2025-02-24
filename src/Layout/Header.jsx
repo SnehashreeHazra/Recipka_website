b4162ecb4bfeb192387b50abfa414313e1ad7199
Snehashree Hazra <snehashreehazra783@gmail.com>
2025-02-24 13:49:43 +0530

    up

--- src/Layout/Header.jsx
+++ src/Layout/Header.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import headerLogo from '../assets/r logo.png'
+import { NavLink } from 'react-router-dom'
 
 const Header = () => {
   return (
@@ -10,7 +11,7 @@ const Header = () => {
       </div>
       <div className="header-right">
         <p><i class="ri-home-4-line"></i><span>Home</span></p>
-        <p><i class="ri-phone-line"></i><span>Contact Us</span></p>
+        <p><NavLink to="/contact-us"><i class="ri-phone-line"></i><span>Contact Us</span></NavLink></p>
       </div>
     </div>
     </div>
